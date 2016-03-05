3ac0235bf12d827fb122b73e799b7c1975cb3a8b
satyav7 <satyav7@missionrnd.com>
2016-03-05 17:11:32 +0530

    Submit Commit

--- src/isOlder.cpp
+++ src/isOlder.cpp
@@ -12,7 +12,18 @@ ERROR CASES: Return -1 if any DOB is invalid.
 
 NOTES: 		Don't use any built-in C functions for comparisions. You are free to write any helper functions.
 */
-
+#include<stdio.h>
+bool check(char[], char[]);
 int isOlder(char *dob1, char *dob2) {
+	/*bool res;
+	res=check(dob1, dob2);*/
 	return 0;
 }
+bool check(char *dob1,char *dob2){
+	int i;
+	for (i = 0; dob1[i] != '\0'; i++)
+		
+	return true;
+	
+}
+
